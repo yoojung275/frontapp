c24c663f67365d389fbdd702b1a8088c9b643319
yoojung275 <yoojung275@gmail.com>
2024-03-28 12:50:56 +0900

    메인페이지 작업 중

---
+++ js/common.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", foundation() {
+    const swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+          },
+      });
+});
+
